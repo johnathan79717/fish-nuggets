ece1975ffb465b399e851c48e2ceebecaffec16e
Andrew Liu <andrewliu33@gmail.com>
2011-09-04 18:39:53 +0800

    Cleaning up config.fish

--- config.fish
+++ config.fish
@@ -1,20 +1,13 @@
-for p in /usr/bin /usr/local/bin /usr/local/mysql/bin ~/.gem/bin ~/.gem/ruby/1.8/bin ~/bin ~/.config/fish/bin 
-	if test -d $p
-		set -x PATH $p $PATH
-	end
+for p in /usr/bin /usr/local/bin ~/bin ~/.config/fish/bin 
+  if test -d $p
+    set -x PATH $p $PATH
+  end
 end
 
 set -x EDITOR "vim"
-
-set fish_greeting ""
-set -x CLICOLOR 1
-
 set -x BROWSER open
-
-cd ~
-
-set -x JAVA_HOME "/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home"
-
+set -x CLICOLOR 1
+set fish_greeting ""
 
 # pull in a local, ignored-by-git config file
 if test -f ~/.config/fish/config.local.fish

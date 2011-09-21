e73ba4db116f88dfea0c54e0d9dce86d980329d4
Andrew Liu <andrewliu33@gmail.com>
2011-09-21 23:07:04 +0800

    Try not to be override by rvm config

--- config.fish
+++ config.fish
@@ -1,14 +1,3 @@
-for p in /usr/bin /usr/local/bin ~/bin
-  if test -d $p
-    set -x PATH $p $PATH
-  end
-end
-
-set -x EDITOR "vim"
-set -x BROWSER open
-set -x CLICOLOR 1
-set fish_greeting ""
-
 # pull in a local, ignored-by-git config file
 if test -f ~/.config/fish/config.local.fish
   . ~/.config/fish/config.local.fish
@@ -31,3 +20,14 @@ if test -f ~/.rvm/scripts/rvm
 
   . ~/.config/fish/rvm.local.fish
 end
+
+for p in ~/bin
+  if test -d $p
+    set -x PATH $p $PATH
+  end
+end
+
+set -x EDITOR "vim"
+set -x BROWSER open
+set -x CLICOLOR 1
+set fish_greeting ""

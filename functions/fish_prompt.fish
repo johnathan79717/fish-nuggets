42123c8114e1b4412bb849dc2b79e0af4af8a1b1
Jonathan Hao <johnathan79717@gmail.com>
2015-11-08 18:14:41 +0800

    Add hostname to fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description "Write out the prompt"
-  printf '%s%s %s%s%s%s%s>' (set_color cyan) (whoami) (set_color green) (prompt_pwd) (parse_git_branch) (set_color white)
+  printf '%s%s@%s %s%s%s%s%s>' (set_color cyan) (whoami) (hostname) (set_color green) (prompt_pwd) (parse_git_branch) (set_color white)
   set_color normal
   printf ' '
 end

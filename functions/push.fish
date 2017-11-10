7e609df423467e834830b88f3a7dab330a8712b9
Jonathan Hao <jonathan.hao@booking.com>
2017-11-10 16:22:14 +0100

    push as a function

--- functions/push.fish
+++ functions/push.fish
@@ -1,10 +1,12 @@
-while not git push
-    git fetch origin trunk
-    if stg rebase origin/trunk
-        git clean -df ~/workspace/main
-        stg clean
-    else
-        break
+function push
+    while not git push
+        git fetch origin trunk
+        if stg rebase origin/trunk
+            git clean -df ~/workspace/main
+            stg clean
+        else
+            break
+        end
     end
+    stg commit -a
 end
-stg commit -a
